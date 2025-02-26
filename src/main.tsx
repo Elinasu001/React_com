48d0a7e5df5fb7536a91a8b2ba487473c09f6524
suelina001 <elinaen001@gmail.com>
2025-02-26 14:50:30 +0900

    [퍼블] :: sass 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import App from '@src/App';
 import '@src/assets/styles/css/index.css';
+import '@src/assets/styles/scss/global.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
