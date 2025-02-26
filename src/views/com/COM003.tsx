163b1b7fc2b4e95b0128acf4dedf415ab0cc212c
namkyo <hipen1@gnbsoftec.co.kr>
2025-02-26 15:44:35 +0900

    화면추가

---
+++ src/views/com/COM003.tsx
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview [공통] 
+ *
+ * @author 
+ * @version 1.0.0
+ */
+
+import { Box } from "@mui/material";
+
+const COM003 = () => {
+  return (
+    <Box
+    sx={{
+      textAlign: 'center'}}
+    >
+      공통
+    </Box>
+  );
+};
+
+export default COM003;
---
+++ src/views/com/COM005.tsx
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview [공통] 
+ *
+ * @author 
+ * @version 1.0.0
+ */
+
+import { Box } from "@mui/material";
+
+const COM005 = () => {
+  return (
+    <Box
+    sx={{
+      textAlign: 'center'}}
+    >
+      공통
+    </Box>
+  );
+};
+
+export default COM005;
