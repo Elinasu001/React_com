4394f0a33969bf0a6020c472e458d1e5f6174b9f
suelina001 <elinaen001@gmail.com>
2025-02-26 13:29:15 +0900

    [퍼블] :: 아이프레임 임시유지, 팝업 내용 텍스트 삭제제

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -72,7 +72,6 @@ export const openBottomPopup = (prop: PopupProps) => {
             <div className="pop-body">
               {/* 팝업 내용 */}
               <iframe src={prop.url} className="popup-iframe"></iframe>
-              팝업 내용
             </div>
             
             {/* 버튼 */}
@@ -152,8 +151,7 @@ export const openFullPopup = (prop: PopupProps) => {
 
             <div className="pop-body">
               {/* 팝업 내용 */}
-              {/* <iframe src={prop.url} className="popup-iframe"></iframe> */}
-              팝업 내용
+              <iframe src={prop.url} className="popup-iframe"></iframe>
             </div>
 
             {/* 닫기 버튼 */}
@@ -235,7 +233,6 @@ export const openPopup = (prop: PopupProps) => {
             <div className="pop-body">
               {/* 팝업 내용 */}
               <iframe src={prop.url} className="popup-iframe"></iframe>
-              팝업 내용
             </div>
             
           </div>
