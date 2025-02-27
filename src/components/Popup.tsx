b02f6dc0bf117ff20f36defb019fbba8204412ec
namkyo <hipen1@gnbsoftec.co.kr>
2025-02-27 17:59:31 +0900

    델피노 테스트

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -344,4 +344,69 @@ export const openWebPopup = (url: string, title: string, nFunc?: (data : DataSet
   );
 };
 
-export default { openPopup, openBottomPopup, openFullPopup, openFullPopup2, openBottomPopup2, openWebPopup };
+/**
+ * 타 웹 호출 팝업
+ * @param param0 
+ */
+export const openHtmlPopup = (url: string, title: string, nFunc?: (data : DataSet) => void) => {
+  const formId = 'gOpenHtmlPopup';
+  document.getRoot(formId).render(
+    React.createElement(() => {
+
+      //팝업상태
+      const [open, setOpen] = useState(false);
+      
+      //팝업 컴포넌트 생성후 처리
+      useEffect(() => {
+        setOpen(true);
+        const iframeCallBack = (event: MessageEvent) => {
+            popupClose(new DataSet(event.data))
+        };
+        window.addEventListener("message", iframeCallBack);
+        return () => {
+            window.removeEventListener("message", iframeCallBack);
+        };
+      }, []);
+
+      //팝업 컴포넌트 닫기 처리
+      const popupClose = (data?: DataSet) => {
+        setOpen(false);
+        setTimeout(() => {
+          document.removeRoot(formId);
+          nFunc?.(new DataSet(data));
+        }, 300);
+      };
+
+      //팝업 컴포넌트 생성
+      return (
+        <MemoryRouter>
+          <Modal open={open} onClose={() => { popupClose(); }}>
+            <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+              <Box className="popup-container full">
+                <Box className="pop-header">
+                  <Typography variant="h2" className="pop-tit">{title}</Typography>
+                  {/* X 닫기 버튼 */}
+                  <Button aria-label="close" onClick={() => { popupClose(); }} className="btn btn-close right">
+                    <Typography component="span" className="sr-only">닫기</Typography>
+                  </Button>
+                </Box>
+                <Box className="pop-body">
+                  
+                  {/* 팝업 내용 */}
+                  <iframe
+                      src={url}
+                      width='100%'
+                      height='100%'
+                      style={{ border: "none" }}
+                  />
+                </Box>
+              </Box>
+            </Slide>
+          </Modal>
+        </MemoryRouter>
+      );
+    })
+  );
+};
+
+export default { openPopup, openBottomPopup, openFullPopup, openFullPopup2, openBottomPopup2, openWebPopup, openHtmlPopup };
