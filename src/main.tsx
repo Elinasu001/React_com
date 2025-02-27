b2e5b54f5624ad3147fee6ebfd5072fa031aea66
suelina001 <elinaen001@gmail.com>
2025-02-27 10:47:34 +0900

    [퍼블] marterial 테마 기본 세팅 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { store } from "@assets/js/common_store"; // store 불러오기
+import { ThemeProvider } from "@mui/material/styles";
 import App from '@src/App';
 import '@src/assets/styles/scss/global.scss';
 import '@src/index.css';
+import theme from "@src/theme/theme";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -10,7 +12,9 @@ import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
