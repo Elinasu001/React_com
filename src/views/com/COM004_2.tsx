140fdbc1bf5a53acbaa264833281aec0c6e5671f
이리라 <krkjseichi0903@gmail.com>
2025-02-27 17:49:04 +0900

    계좌인증팝업수정

--- src/views/com/COM004_2.tsx
+++ src/views/com/COM004_2.tsx
@@ -1,41 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Box, Typography, Button, TextField, MenuItem } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { GLog,doAction, makeForm, addFormData  } from '@assets/js/common';
 import { progressBar } from "@src/components/Loading";
 import { messageView } from '@src/components/Alert';
 import { NumberBox } from "@src/components/Input";
 import { Button01 } from "@src/components/Button";
-
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Logo from "@assets/images/com/svg/img_accountCrtf.png";
-
-interface DataSet {
-  bankCode: string; 
-  bankName: string;  
-}
+import DataSet from "@assets/io/DataSet";
 
 const COM004_2 = () => {
   
   const [number, setNumber] = useState('');
-  const [selectedBankCd, setSelectedBankCd] = useState<string>("");
-  const [selectedBankNm, setSelectedBankNm] = useState<string>("");
-  const [formData, setFormData] = useState("");
-
-  
-  function selectBankCd(data?: DataSet) {
-    if (!data || !("bankCode" in data)) {
-      setSelectedBankCd(""); 
-      setSelectedBankNm(""); 
-      return;
-    }else {
-      setSelectedBankCd(data.bankCode as string);
-      setSelectedBankNm(data.bankName as string);
-    }
-  };
-  
-
     
   // 인증번호받기 이벤트 
   const fsbAcnoConfirmAuth = async () => { 
@@ -43,22 +19,22 @@ const COM004_2 = () => {
     //폼생성,데이터 주입
     const form = makeForm('COM0004SC');
     addFormData(form,'txGbnCd','A02');
-    addFormData(form,'CUSTNM', "");
+    addFormData(form,'AUTN_STR', number);
     
     //로딩 ON
     progressBar(true, "통신중");
 
     //통신
-    const test01 = await doAction(form);
+    const resDs = await doAction(form);
 
     //로딩 OFF
     progressBar(false);
     
     //결과실패
-    if(test01.header.respCd != 'N00000'){
+    if(resDs.header.respCd != 'N00000'){
     GLog.e('에러발생 !!!');
     messageView(
-        '통신 실패 : '+test01.header.respMsg,
+        '통신 실패 : '+resDs.header.respMsg,
         '확인',
         () => GLog.d('확인 클릭')
     )
@@ -66,15 +42,25 @@ const COM004_2 = () => {
     }
 
     //정상
-    messageView(
-    '정상처리되었습니다',
-    '확인',
-    (() => {
-        // TODO컨펌화면으로 이동
-    })
-    
-    )
-    
+    if(resDs.data.getString('AUTH_SUCC_YN') === 'N'){
+      messageView(
+        '입력하신 인증번호가 일치하지 않아요',
+        '확인',
+        (() => {
+            // TODO 팝업닫기
+        })
+        
+        )
+    }else {
+      messageView(
+        '계좌인증을 완료했어요',
+        '확인',
+        (() => {
+            // TODO 팝업닫기
+        })
+        
+        )
+    }
 
   };
   
@@ -96,8 +82,8 @@ const COM004_2 = () => {
              <NumberBox label="숫자 입력" value={number} onChange={(e) => setNumber(e.target.value)} />
           </Box>
 
-          <InputLabel id="demo-simple-select-helper-label"><strong>입금자명 뒤쪽 4자리 숫자 입력하세요</strong></InputLabel>
-            <Button01 btnName = '계좌인증'clickFunc={fsbAcnoConfirmAuth}/>
+          <InputLabel id="demo-simple-select-helper-label"><strong>입력시간:타이머</strong></InputLabel>
+          <Button01 btnName = '확인'clickFunc={fsbAcnoConfirmAuth}/>
       </Box>
  
   );
