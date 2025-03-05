cbcafe144c3f823c45e4ec1f779ffcd2e60c1cd3
suelina001 <elinaen001@gmail.com>
2025-03-05 11:20:44 +0900

    [퍼블] 정리중

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -67,7 +67,7 @@ export const TextBox02 = ({ text }: TextBoxProps) => {
  */
 interface ContentTitleProps {
   title?: React.ReactNode; // h3 상단 타이틀 (선택적)
-  desc?: string;   // 설명 텍스트 (선택적)
+  desc?: React.ReactNode;   // 설명 텍스트 (선택적)
 }
 
 /**
@@ -80,12 +80,14 @@ export const ContentTitle = ({ title, desc }: ContentTitleProps) => {
      * title[default(큰타이틀)] or desc[설명 텍스트] 선택 사용
      * 예시)
      * <ContentTitle
-     *     <>
-     *       <strong>출금계좌번호</strong>의
-     *       <br />
-     *       <strong>보안카드 번호</strong>를 입력해 주세요
-     *     </>
-     *     desc="보안카드를 입력해주세요. 5회 오류 시 서비스가 제한됩니다."
+     *     title={
+     *      <>
+     *        <strong>출금계좌번호</strong>의
+     *        <br />
+     *        <strong>보안카드 번호</strong>를 입력해 주세요
+     *      </>
+    *      }
+     *     desc="보안카드를 입력해주세요. 5회 오류 시 서비스가 제한됩니다." 또는 위 예시 title 형식으로도 가능
      *  />
     **/
 
