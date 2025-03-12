54218bb9ec60bbe4c34ba1b6562b97315f4564b9
suelina001 <elinaen001@gmail.com>
2025-03-12 09:53:45 +0900

    탭 스와이퍼 플러그인 추가

--- src/assets/html/00_common/Tab.tsx
+++ src/assets/html/00_common/Tab.tsx
@@ -6,6 +6,7 @@
  */
 import { Box, Tab, Tabs } from "@mui/material";
 import React, { ReactNode, useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
 /**
  * 개별 탭 항목 속성
@@ -20,60 +21,62 @@ interface TabItem {
  * 공통 탭 컴포넌트 속성
  */
 interface TabProps {
-  items: TabItem[];                             // 탭 항목 배열
-  initialValue?: string | number;               // 기본 선택 탭 (value)
-  borderBottom?: boolean;                       // 탭 하단 테두리 여부
-  isScrollable?: boolean;                       // 탭 스크롤 가능 여부
-  containerClass?: "bg-gray" | "round" | "full" | "";    // 추가할 클래스 (bg-gray, round)
-  scrollButtons?: true | false | "auto";        // 스크롤 버튼 표시 방식
-  allowScrollButtonsMobile?: boolean;           // 모바일에서 스크롤 버튼 활성화 여부
-  ariaLabel?: string;                           // 접근성을 위한 Aria Label
-  onChange?: (value: string | number) => void;  // 탭 변경 콜백
+  ariaLabel?: string;                                    // 접근성을 위한 Aria Label
+  initialValue?: string | number;                        // 기본 선택 탭 (value)
+  items: TabItem[];                                      // 탭 항목 배열
+  containerClass?: "bg-gray" | "round" | "full" | "";    // 추가할 클래스 (bg-gray, round, full)
+
+  scrollButtons?: true | false | "auto";                 // 스크롤 버튼 표시 여부
+  isScrollable?: boolean;                                // 탭 스크롤 가능 여부
+  allowScrollButtonsMobile?: boolean;                    // 모바일에서 스크롤 버튼 활성화 여부
+  useSwiper?: boolean;                                   // 스와이프 적용 여부
+  onChange?: (value: string | number) => void;           // 탭 변경 콜백
 }
 
-
-/**
- *  탭 컴포넌트
- *  // 스타일
- *  기본 탭
- *  꽉찬 탭 :: containerClass="full"
- *  배경 탭 :: containerClass="bg-gray"
- *  라운드 탭 :: containerClass="round"
- *  // 기능
- *  탭 스크롤 기능 :: Tabs 태그 [ isScrollable={true} 적용 ]
- *  탭 스크롤 기능 + 버튼 기능 :: Tabs 태그 [ isScrollable={true} + scrollButtons={true} 적용 ]
- *
- *  예시)
- *  <Tab01
- *     items={[
- *       { value: "tab1", label: "탭 1", component: <div>내용 1</div> },
- *       { value: "tab2", label: "탭 2", component: <div>내용 2</div> },
- *     ]}
- *     initialValue="기본 선택 탭"
- *     isScrollable={true}  // 스크롤 추가
- *     containerClass="bg-gray" // "bg-gray", "round" "full" 스타일
- *   />
- **/
-
-export const Tab01 = ({
-  items,
-  initialValue,
-  isScrollable = false,
-  containerClass = "",
-  scrollButtons = "auto",
-  allowScrollButtonsMobile = false,
+export const Tab01 = ({                                  // 기본값 설정
   ariaLabel = "탭 UI",
+  initialValue,
+  items,
+  containerClass = "",
+
+  scrollButtons = "auto",
+  isScrollable = false,
+  allowScrollButtonsMobile = false,
+  useSwiper = false,
   onChange,
 }: TabProps) => {
   const [value, setValue] = useState(initialValue ?? items[0]?.value); // 기본값이 없으면 첫 번째 탭 선택
+  const currentIndex = items.findIndex((item) => item.value === value);
 
   const handleChange = (_: React.SyntheticEvent, newValue: string | number) => {
     setValue(newValue);   // 선택한 탭
     if (onChange) onChange(newValue);
   };
 
+  // 스와이프 이벤트 핸들러
+  const handlers = useSwiper
+    ? useSwipeable({
+        onSwipedLeft: () => {
+          if (currentIndex < items.length - 1) {
+            const newValue = items[currentIndex + 1].value;
+            setValue(newValue);
+            if (onChange) onChange(newValue);
+          }
+        },
+        onSwipedRight: () => {
+          if (currentIndex > 0) {
+            const newValue = items[currentIndex - 1].value;
+            setValue(newValue);
+            if (onChange) onChange(newValue);
+          }
+        },
+        preventScrollOnSwipe: true, // preventDefaultTouchmoveEvent 버그 해결  > preventScrollOnSwipe 최신 버전으로 적용
+        trackMouse: true, // 마우스 드래그 지원
+      })
+    : {};
+
   return (
-    <Box className={`tab-container ${containerClass}`.trim()}>
+    <Box className={`tab-container ${containerClass}`.trim()} {...handlers}>
       {/* 탭 버튼 리스트 */}
       <Tabs
         value={value}
@@ -102,3 +105,27 @@ export const Tab01 = ({
 };
 
 export default { Tab01 };
+
+
+/**
+ *  탭 컴포넌트
+ *  // 스타일
+ *  기본 탭
+ *  꽉찬 탭 :: containerClass="full"
+ *  배경 탭 :: containerClass="bg-gray"
+ *  라운드 탭 :: containerClass="round"
+ *  // 기능
+ *  탭 스크롤 기능 :: Tabs 태그 [ isScrollable={true} 적용 ]
+ *  탭 스크롤 기능 + 버튼 기능 :: Tabs 태그 [ isScrollable={true} + scrollButtons={true} 적용 ]
+ *
+ *  예시)
+ *  <Tab01
+ *     items={[
+ *       { value: "tab1", label: "탭 1", component: <div>내용 1</div> },
+ *       { value: "tab2", label: "탭 2", component: <div>내용 2</div> },
+ *     ]}
+ *     initialValue="기본 선택 탭"
+ *     isScrollable={true}  // 스크롤 추가
+ *     containerClass="bg-gray" // "bg-gray", "round" "full" 스타일
+ *   />
+ **/
--- src/assets/html/00_common_page/Tab_page.tsx
+++ src/assets/html/00_common_page/Tab_page.tsx
@@ -8,9 +8,19 @@ const Tab_page = () => {
   
   //예시 데이터
   const noticeListData = [
-    { id: 1, label: "2023년 2분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지", date: "2023.04.18", icon: true },
-    { id: 2, label: "2023년 2분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지", date: "2023.04.18", icon: true },
-    { id: 3, label: "2023년 2분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지", date: "2023.04.18" },
+    { id: 1, label: "2023년 2분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지1", date: "2023.04.18", icon: true },
+    { id: 2, label: "2023년 2분기 5월 캠코매각 대상 (개인) 신용정보 제공 사실 공지2", date: "2023.04.18", icon: true },
+    { id: 3, label: "2023년 2분기 6월 캠코매각 대상 (개인) 신용정보 제공 사실 공지3", date: "2023.04.18" },
+  ];
+
+  const noticeListData02 = [
+    { id: 1, label: "2023년 3분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지1", date: "2023.04.18", icon: true },
+    { id: 2, label: "2023년 3분기 5월 캠코매각 대상 (개인) 신용정보 제공 사실 공지2", date: "2023.04.18", icon: true },
+  ];
+
+  const noticeListData03 = [
+    { id: 1, label: "2023년 4분기 4월 캠코매각 대상 (개인) 신용정보 제공 사실 공지1", date: "2023.04.18", icon: true },
+    { id: 3, label: "2023년 4분기 6월 캠코매각 대상 (개인) 신용정보 제공 사실 공지3", date: "2023.04.18" },
   ];
 
 //예시 데이터
@@ -38,11 +48,22 @@ const productsData = [
     onClick: () => console.log("상품 클릭됨"),
   }
 ];
-
-
-
     return (
       <>
+
+        <Typography className="exp">탭 _ Swiper</Typography>
+
+        <Tab01
+          items={[
+            { value: "tab1", label: "탭 1", component: <NotiList items={noticeListData}/>},
+            { value: "tab2", label: "탭 2", component: <NotiList items={noticeListData02}/> },
+            { value: "tab3", label: "탭 3", component: <NotiList items={noticeListData03}/>},
+          ]}
+          initialValue="tab1"
+          isScrollable={true}   // 스크롤 기능
+          useSwiper={true}      // 스와이프 기능 활성화
+        />
+
         <Typography className="exp">탭 _ 일반[origin]</Typography>
 
         <Tab01
