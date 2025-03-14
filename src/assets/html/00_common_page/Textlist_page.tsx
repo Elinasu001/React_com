d8bd414f6f1396cd20dd3f16d169e283847100cf
suelina001 <elinaen001@gmail.com>
2025-03-14 13:42:19 +0900

    타이틀 추가

--- src/assets/html/00_common_page/Textlist_page.tsx
+++ src/assets/html/00_common_page/Textlist_page.tsx
@@ -29,6 +29,28 @@ const Textlist_page = () => {
         listIcon={true}
         // hideTitle={true} // titleIcon 사용시 삭제
       />
+
+    <Typography className="exp">정보 리스트 - dot</Typography>
+      {/* 정보 - 타이틀 있을 경우 */}
+      <InfoList
+        title="보안카드 입력 안내"
+        items={[
+          "5회 오류 시 서비스 이용이 중단되고 메인으로 이동해요",
+          "오류 해제를 원하실 경우 영업점에 방문해 이용제한 해제를 하셔야 해요"
+        ]}
+        hideTitle={true}
+      />
+
+    <Typography className="exp">정보 리스트 - dot - 타이틀</Typography>
+      {/* 정보 - 타이틀 있을 경우 */}
+      <InfoList
+        title="보안카드 입력 안내"
+        items={[
+          "5회 오류 시 서비스 이용이 중단되고 메인으로 이동해요",
+          "오류 해제를 원하실 경우 영업점에 방문해 이용제한 해제를 하셔야 해요"
+        ]}
+        // hideTitle={true}
+      />
       <Typography className="exp">계좌 관리 상세 리스트 <br/>** 작업중입니다.</Typography>
       
       </>
