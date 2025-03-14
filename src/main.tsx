49b1203cf85ab12c8da81c6f792948b9c42f3869
suelina001 <elinaen001@gmail.com>
2025-03-14 10:18:15 +0900

    22222

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import { ThemeProvider } from "@mui/material/styles";
-import App from '@src/App';
 import { store } from "@src/assets/js/redux/store"; // store 불러오기
+// import { ThemeProvider } from "@mui/material/styles";
+import App from '@src/App';
 import '@src/assets/styles/scss/global.scss';
 import '@src/index.css';
-import theme from "@src/theme/theme";
+// import theme from "@src/theme/theme";
+import { StyledEngineProvider } from '@mui/styled-engine';
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <StyledEngineProvider injectFirst>
       <App />
-    </ThemeProvider>
+    </StyledEngineProvider>
   </Provider>
 );
 // 밑에 로직은 StrictMode는 개발 모드에서 의도적으로 컴포넌트를 두 번 렌더링하여 부작용을 감지하는 기능
